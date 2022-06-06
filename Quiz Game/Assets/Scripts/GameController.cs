25ba295f5cd9e273b2881c775d11d5f2bd894fdd
StvnDugz <stvndugz@gmail.com>
2022-06-06 13:08:28 +0100

    Added a game controller script
    Added a game controller script which controls the functionality of the game

---
+++ Quiz Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+    public Text questionDisplayText;
+    public SimpleObjectPool answerButtonObjectPool;
+    public Transform answerButtonParent;
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+    private bool isRoundActive;
+    private int questionIndex;
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        dataController = FindObjectOfType<DataController>();
+        currentRoundData = dataController.GetCurrentRoundData();
+        questionPool = currentRoundData.questions;
+
+        questionIndex = 0;
+
+        ShowQuestion();
+        isRoundActive = true;
+
+    }
+
+    private void ShowQuestion()
+    {
+        RemoveAnswerButtons();
+        QuestionData questionData = questionPool[questionIndex];
+        questionDisplayText.text = questionData.questionText;
+
+        for (int i = 0; i < questionData.answers.Length; i++)
+        {
+            GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
+            answerButtonGameObjects.Add(answerButtonGameObject);
+            answerButtonGameObject.transform.SetParent(answerButtonParent, false);
+
+            AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+            answerButton.Setup(questionData.answers[i]);
+        }
+    }
+
+    private void RemoveAnswerButtons()
+    {
+        while (answerButtonGameObjects.Count > 0)
+        {
+            answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
