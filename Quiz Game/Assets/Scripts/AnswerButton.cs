f58d08f54c0f121fbc2db8631485bb513aeca248
StvnDugz <stvndugz@gmail.com>
2022-06-06 12:08:58 +0100

    Added an answer button script
    Added an answer button script and created a button to display the answers

---
+++ Quiz Game/Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+
+    public Text answerText;
+
+    private AnswerData answerData;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void Setup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.answerText;
+    }
+}
