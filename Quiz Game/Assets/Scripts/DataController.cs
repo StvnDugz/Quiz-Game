1dad309a2bcb311cd6353c98d0874f87ebd6dfd0
StvnDugz <stvndugz@gmail.com>
2022-06-09 10:58:33 +0100

    Updated data controller to be able to read data from JSON
    Updated data controller to be able to read questions from a JSON file

--- Quiz Game/Assets/Scripts/DataController.cs
+++ Quiz Game/Assets/Scripts/DataController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class DataController : MonoBehaviour
 {
@@ -9,11 +10,15 @@ public class DataController : MonoBehaviour
 
     private PlayerProgress playerProgress;
 
+    private string gameDataFileName = "data.json";
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        SceneManager.LoadScene("Menu");
+        //LoadGameData();
         LoadPlayerProgress();
+        SceneManager.LoadScene("Menu");
+
     }
 
     // Update is called once per frame
@@ -54,9 +59,31 @@ public class DataController : MonoBehaviour
         }
     }
 
-        private void SavePlayerProgress()
+    private void SavePlayerProgress()
     {
         // Save the value playerProgress.highestScore to PlayerPrefs, with a key of "highestScore"
         PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
+    }
+
+    private void LoadGameData()
+    {
+        // Path.Combine combines strings into a file path
+        // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(filePath);
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            // Retrieve the allRoundData property of loadedData
+            allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
     }
 }
